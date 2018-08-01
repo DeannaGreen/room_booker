c9569c6ed63b534aa0deef33cc9ee0f98fbcf406
Matthew Green <mg@16vmini.co.uk>
2018-08-01 16:11:12 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'rooms#index'
   resources :rooms do
     member do
-      get member '/rooms/:id/panel', to: 'rooms#panel'
+      get  '/rooms/:id/panel', to: 'rooms#panel'
       get '/rooms/:id/bookings', to: 'rooms#bookings'
     end
   end 
