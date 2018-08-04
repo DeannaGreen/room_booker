37a894b4b5ce0d55711dbf613a4d441b1876ca5d
DeannaGreen <deanna@16vmini.co.uk>
2018-08-04 20:09:58 +0100

    joined tables

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,6 @@
 class Room < ApplicationRecord
+  # This creates a method on room called #roombookings
+  # It checks the roombookings table for records with a room_id that matches the room instance
+  # and returns them in an activerecord collection (basically a fancy array)
+  has_many :roombookings
 end
--- app/models/roombooking.rb
+++ app/models/roombooking.rb
@@ -1,2 +1,5 @@
 class Roombooking < ApplicationRecord
+  # Creates a method called room on the roombooking instance
+  # This will search the Room table, it will return the record with an id that matches the room_id field
+  belongs_to :room
 end
