535f95431ee8e1dfb99e91bea9234bac559d930d
DeannaGreen <deanna@16vmini.co.uk>
2018-08-28 20:11:17 +0100

    fixing booking page under rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,7 +16,8 @@ class RoomsController < ApplicationController
   
   def bookings
 
-    @bookings = Roombooking.includes(:room,:user).where(room_id:@room.id).group_by(&:room)
+    @bookings = Roombooking.includes(:room,:user).where(room_id: @room.id)
+    # .group_by(&:room)
     respond_to do |format|
       format.html
       format.json
