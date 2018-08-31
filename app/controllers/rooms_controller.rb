0c06c823a0d6c5d09f7a6b17b84b3ed9685ad82e
Matthew Green <mg@16vmini.co.uk>
2018-08-31 08:31:51 +0100

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update, :destroy , :panel ,:bookings]
+  before_action :set_room, only: [:show, :edit, :update, :destroy , :panel ,:bookings,:jsonbookings]
  
   
   # GET /rooms
