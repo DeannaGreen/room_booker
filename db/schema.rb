9559ad0d92cb56a586e5cb21b33369cb0cdd9722
DeannaGreen <deanna@16vmini.co.uk>
2018-09-03 16:07:07 +0100

    added company table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_181218) do
+ActiveRecord::Schema.define(version: 2018_09_03_150515) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "companyname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string "paragraph_1"
