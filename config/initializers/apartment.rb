5cfcfed7f3b8625b9cb34686cccd1cd59498136c
Matthew Green <mg@16vmini.co.uk>
2018-10-01 08:44:16 +0100

    Update apartment.rb

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -104,7 +104,7 @@ end
 # }
 
 # Rails.application.config.middleware.use Apartment::Elevators::Domain
-Rails.application.config.middleware.use Apartment::Elevators::Subdomain
+# Rails.application.config.middleware.use Apartment::Elevators::Subdomain
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
 
