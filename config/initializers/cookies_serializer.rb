6f5cf0a932c582a343ae47dcdc6cdc977dee38de
Matthew Green <mg@16vmini.co.uk>
2018-10-01 09:24:05 +0100

    Update cookies_serializer.rb

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -4,7 +4,7 @@
 # Valid options are :json, :marshal, and :hybrid.
 Rails.application.config.action_dispatch.cookies_serializer = :json
 
-Rails.application.config.session_store :cookie_store, :key => '_room_book_session', :domain => ".lvh.me"
+Rails.application.config.session_store :cookie_store, :key => '_room_book_session', :domain => ".happymuffin.co.uk"
 
 #domain: {
 #  production:   '.myapp.com',
