917611b1689fde9d4623eefe16672726cdeda56d
DeannaGreen <deanna@16vmini.co.uk>
2018-10-01 18:45:28 +0100

    adding admin field

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,11 +3,11 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :subdomain)
+    params.require(:user).permit(:email, :password, :password_confirmation, :subdomain, :admin)
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :subdomain)
+    params.require(:user).permit(:email, :password, :password_confirmation, :current_password, :subdomain, :admin)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_18_190659) do
+ActiveRecord::Schema.define(version: 2018_10_01_173906) do
 
   create_table "companies", force: :cascade do |t|
     t.string "companyname"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2018_09_18_190659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "subdomain"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
