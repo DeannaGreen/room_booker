bfa57a5da068e8a3b6c4285423a5f41d8c7c9705
Matthew Green <mg@16vmini.co.uk>
2018-10-03 15:40:43 +0100

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -40,6 +40,9 @@ def jsonbookings
   # GET /rooms/1/panel
   def panel
     @bookings = Roombooking.includes(:user).where(room_id: @room.id)
+    if (@bookings.nil?)
+      @bookings = Booking.New
+    end
     @nextBooking = Roombooking.includes(:user).where(room_id: @room.id).where("endtime >= ?", DateTime.now).order(endtime: :desc).first
     @currentBooking = Roombooking.includes(:user).where(room_id: @room.id).where("starttime <= ? and endtime >= ?", DateTime.now,DateTime.now).order(endtime: :desc).first
    if (@currentBooking.nil?)
