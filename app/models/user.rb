e974dae3a3c5f6a967ba746b957c8777de408866
Matthew Green <mg@16vmini.co.uk>
2018-10-08 16:20:08 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_validation :downcase_subdomain
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :roombookings
+  
+  
+	private
+  def downcase_subdomain
+            self.subdomain = subdomain.try(:downcase) 
+  end 
+  
 end
